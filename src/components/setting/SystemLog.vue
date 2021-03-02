6aab9133796b887d493dd7a359f5b8fae3bf6f1d
dqw <761945125@qq.com>
2021-03-02 18:47:08 +0800

    update log

--- src/components/setting/SystemLog.vue
+++ src/components/setting/SystemLog.vue
@@ -118,11 +118,36 @@ export default {
       },
     }
   }, methods: {
-    info() {
-
+    info(row) {
+      alert("敬请期待")
     },
-    deleteRow() {
-
+    deleteRow(row) {
+      this.$confirm('此操作将永久删除该数据, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$axios.post("/system/log/delete", {
+          "id": row.id
+        }).then(res => {
+          let da = res.data;
+          if (da) {
+            this.$message({
+              showClose: true,
+              message: '删除成功',
+              type: 'success'
+            });
+            this.list();
+          }
+        }).catch(function (error) {
+          console.log(error);
+        });
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
     },
     reset(formName) {
       this.$refs[formName].resetFields();
